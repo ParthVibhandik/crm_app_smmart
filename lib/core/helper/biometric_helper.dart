c2bc7b55ac8bd747438e49319a02587c601121f5
DHAVALMODI1625 <Dhavalmodi1625@gmail.com>
2025-12-12 16:02:00 +0530

    fix: disable biometrics on web to prevent crashes

--- lib/core/helper/biometric_helper.dart
+++ lib/core/helper/biometric_helper.dart
@@ -1,26 +1,30 @@
 import 'package:local_auth/local_auth.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class BiometricHelper {
   static final _auth = LocalAuthentication();
 
   static Future<bool> hasBiometrics() async {
+    if (kIsWeb) return false;
     try {
       return await _auth.canCheckBiometrics || await _auth.isDeviceSupported();
-    } on PlatformException catch (_) {
+    } catch (_) {
       return false;
     }
   }
 
   static Future<List<BiometricType>> getAvailableBiometrics() async {
+    if (kIsWeb) return <BiometricType>[];
     try {
       return await _auth.getAvailableBiometrics();
-    } on PlatformException catch (_) {
+    } catch (_) {
       return <BiometricType>[];
     }
   }
 
   static Future<bool> authenticate({required String localizedReason}) async {
+    if (kIsWeb) return false;
     try {
       return await _auth.authenticate(
         localizedReason: localizedReason,
@@ -30,7 +34,7 @@ class BiometricHelper {
           useErrorDialogs: true,
         ),
       );
-    } on PlatformException catch (_) {
+    } catch (_) {
       return false;
     }
   }
